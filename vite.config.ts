33bb2e00db5fad9652769ad94b452124c7f4f9eb
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-29 11:45:13 -0300

    fix: update base path in Vite configuration to match project structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/2024",
+  base: "/star-week/",
   server: {
     host: "0.0.0.0",
   },
